bf445bd3358d48a7e38a396269cc0655333ed021
Sudharsan <155707028+sudharsanKEC@users.noreply.github.com>
2026-04-13 12:01:34 +0530

    Backend: MongoDB connection setup (Fixes #8) (#9)
    Okay well done

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 
 app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.listen(5000, () => console.log("Server running on port 5000"));
+app.get("/test", async (req, res) => {
+  const mongoose = require("mongoose");
+  const Test = mongoose.model("Test", { name: String });
+
+  await Test.create({ name: "test data" });
+
+  res.send("Data inserted");
+});
